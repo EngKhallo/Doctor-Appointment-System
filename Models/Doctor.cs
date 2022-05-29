57f4914cb9526259b91614fabda496c155c09ccd
EngKhallo <engkhalid.ict@gmail.com>
2022-05-29 11:05:45 +0300

    Doctors Model

--- Models/Doctor.cs
+++ Models/Doctor.cs
@@ -3,6 +3,15 @@ namespace Doctor_Appointment_System.Models
     public class Doctor
     {
         public int Id { get; set; }
-        public int MyProperty { get; set; }
+        public User User { get; set; } = new();
+        public string Phone { get; set; } = "";
+        public string Specialty { get; set; } = "";
+        public string Picture { get; set; } = "";
+        public string Bio { get; set; } = "";
+        public string Certificate { get; set; } = "";
+        public decimal TicketPrice { get; set; }
+        public bool IsVerified { get; set; }
+        public DateTime CreatedAt { get; set; }
+        
     }
 }
