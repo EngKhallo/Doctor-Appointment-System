4c5465e7e72ae0aa7a5e7034f373ce4ed3ee96a0
EngKhallo <engkhalid.ict@gmail.com>
2022-05-31 09:46:08 +0300

    New - BookingNote Model

---
+++ Models/BookingNote.cs
@@ -0,0 +1,10 @@
+namespace Doctor_Appointment_System.Models;
+public class BookingNote
+{
+    public int Id { get; set; }
+    public int BookingId { get; set; }
+    public Booking? Booking { get; set; } = new();
+    public string Note { get; set; }= "";
+    public DateTime CreatedAt { get; set; }
+
+}
