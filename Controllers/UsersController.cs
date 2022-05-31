130d808c8c8ae9c1a142c281ffa85fe660a34351
EngKhallo <engkhalid.ict@gmail.com>
2022-06-01 00:30:03 +0300

    New - EndPoints : Get, Get(1), Post

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,3 +1,5 @@
+using Data;
+using Doctor_Appointment_System.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Doctor_Appointment_System.Controllers;
@@ -5,10 +7,39 @@ namespace Doctor_Appointment_System.Controllers;
 [Route("[controller]")]
 public class UsersController : ControllerBase
 {
+    private readonly AppointmentsDbContext _context;
+    public UsersController(AppointmentsDbContext context)
+    {
+        _context = context;
+    }
     // GET /users
     [HttpGet]
     public IActionResult GetAll()
     {
-        return Ok("List of all Users");
+        var users = _context.Users.ToList();
+        return Ok(users);
+    }
+
+    // Get /Users/5
+    [HttpGet("{id}")]
+    public IActionResult GetUser(int id)
+    {
+        var user = _context.Users.Find(id);
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(user);
+    }
+
+    // Post:Users
+    [HttpPost]
+    public IActionResult Add([FromBody] User user)
+    {
+        _context.Users.Add(user); // saves data to the pc memory
+        _context.SaveChanges(); // saves changes to the Database
+
+        return Created("", user);
     }
 }
