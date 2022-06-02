2c99ce3b4a2e1a75e6295e045158e8f37ee6a8db
EngKhallo <engkhalid.ict@gmail.com>
2022-06-02 16:25:24 +0300

    Update - JSON Options

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
+using System.Text.Json.Serialization;
 using Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppointmentsDbContext>(config => 
+builder.Services.AddDbContext<AppointmentsDbContext>(config =>
                 config.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
