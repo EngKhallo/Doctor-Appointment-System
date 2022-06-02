1b8ff2dba85a987ea5dce7ec441f22115798b290
EngKhallo <engkhalid.ict@gmail.com>
2022-06-02 14:01:46 +0300

    New - TimeSlot ViewMode

---
+++ ViewModels/TimeSlotViewModel.cs
@@ -0,0 +1,10 @@
+namespace Doctor_Appointment_System.ViewModels
+{
+    public class TimeSlotViewModel
+    {
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public string Description { get; set; } = "";
+        public int MaxAppointments { get; set; }
+    }
+}
