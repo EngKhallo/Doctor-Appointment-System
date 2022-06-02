287bbe4d5fb9e2f796b5918623496b67ce0bfeaf
EngKhallo <engkhalid.ict@gmail.com>
2022-06-02 10:27:57 +0300

    Update - Posting Using ViewModel

--- Controllers/DoctorsController.cs
+++ Controllers/DoctorsController.cs
@@ -1,6 +1,8 @@
 using Data;
 using Doctor_Appointment_System.Models;
+using Doctor_Appointment_System.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Doctor_Appointment_System.Controllers;
 
@@ -18,14 +20,14 @@ public class DoctorsController : ControllerBase
     [HttpGet]
     public IActionResult GetAll()
     {
-        var doctors = _context.Doctors.OrderBy(d => d.Id).ToList();
+        var doctors = _context.Doctors.Include(d => d.User).ToList();
         return Ok(doctors);
     }
 
     [HttpGet("{id}")]
     public IActionResult GetSingle(int id)
     {
-        var doctor = _context.Doctors.Find(id);
+        var doctor = _context.Doctors.Include(d => d.User).SingleOrDefault(d => d.Id == id);
         if (doctor is null)
         {
             return NotFound();
@@ -35,12 +37,24 @@ public class DoctorsController : ControllerBase
     }
 
     [HttpPost]
-    public IActionResult Add([FromBody] Doctor doctor)
+    public IActionResult Add([FromBody] DoctorViewModel doctorViewModel) // Over-posting attack.
     {
+        var doctor = new Doctor
+        {
+            Phone = doctorViewModel.Phone,
+            Specialty = doctorViewModel.Specialty,
+            Bio = doctorViewModel.Bio,
+            Certificate = doctorViewModel.Certificate,
+            CreatedAt = DateTime.UtcNow,
+            Picture = doctorViewModel.Picture,
+            TicketPrice = doctorViewModel.TicketPrice,
+            UserId = 2 // TODO : Use the currently logged in UserId
+        };
         _context.Doctors.Add(doctor);
         _context.SaveChanges();
 
         return Created("", doctor);
     }
+
 
 }
