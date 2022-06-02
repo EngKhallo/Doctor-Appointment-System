2b79045681fc8dbd101556a16bb5ebdaec68c489
EngKhallo <engkhalid.ict@gmail.com>
2022-06-02 16:20:24 +0300

    Update - SchedulesViewModel Modify

--- ViewModels/SchedulesViewModel.cs
+++ ViewModels/SchedulesViewModel.cs
@@ -1,12 +1,17 @@
 using System.ComponentModel.DataAnnotations;
 
 namespace ViewModels;
-    public class SchedulesViewModel
-    {
-        public DayOfWeek Day { get; set; }
+public class SchedulesViewModel
+{
+    public DayOfWeek Day { get; set; }
 
-        [Required]
-        public string Location { get; set; } = "";
+    [Required]
+    public string Location { get; set; } = "";
 
 
-    }
+}
+
+public class ModifyScheduleViewModel : SchedulesViewModel
+{
+    public bool IsAvailable { get; set; }
+}
