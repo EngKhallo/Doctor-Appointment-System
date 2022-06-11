95405538b61e0fad3005d68fbc7ddaedeac444d0
EngKhallo <engkhalid.ict@gmail.com>
2022-06-12 00:58:11 +0300

    New - Bookings Controller

--- Controllers/BookingsController.cs
+++ Controllers/BookingsController.cs
@@ -1,7 +1,13 @@
-namespace Doctor_Appointment_System.Controllers
+using Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Doctor_Appointment_System.Controllers;
+[Route("[controller]")]
+public class BookingsController : ControllerBase
 {
-    public class BookingsController
+    private readonly AppointmentsDbContext _context;
+    public BookingsController(AppointmentsDbContext context)
     {
-        
+        _context = context;
     }
 }
