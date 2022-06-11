e74ca66adfc4d649f7d03fc55932fff95ef9c421
EngKhallo <engkhalid.ict@gmail.com>
2022-06-12 01:44:39 +0300

    New - Bookings ViewModel

---
+++ ViewModels/BookingViewModel.cs
@@ -0,0 +1,8 @@
+namespace Doctor_Appointment_System.ViewModels
+{
+    public class BookingViewModel
+    {
+        public DateTime AppointmentTime { get; set; }
+        public int TimeSlotId { get; set; }
+    }
+}
