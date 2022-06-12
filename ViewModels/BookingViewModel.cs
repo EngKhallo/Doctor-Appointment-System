c08631ac097bd7abcc416f72783abf093e18abad
EngKhallo <engkhalid.ict@gmail.com>
2022-06-12 03:43:52 +0300

    Update - BookingViewModel : Payment Method

--- ViewModels/BookingViewModel.cs
+++ ViewModels/BookingViewModel.cs
@@ -4,5 +4,6 @@ namespace Doctor_Appointment_System.ViewModels
     {
         public DateTime AppointmentTime { get; set; }
         public int TimeSlotId { get; set; }
+        public string PaymentMethod { get; set; } = null!;
     }
 }
