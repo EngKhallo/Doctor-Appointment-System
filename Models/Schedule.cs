50e1dec41a7a7507204c20ae59d92351fd71d474
EngKhallo <engkhalid.ict@gmail.com>
2022-06-15 10:27:21 +0300

    Update - List of TimeSlots

--- Models/Schedule.cs
+++ Models/Schedule.cs
@@ -5,10 +5,12 @@ namespace Doctor_Appointment_System.Models
         public int Id { get; set; }
         public DayOfWeek Day { get; set; } // pre-made enum 
         public string Location { get; set; } = "";
+
         public int DoctorId { get; set; }
         public Doctor Doctor { get; set; } = null!;
+        
         public bool IsAvailable { get; set; }
-        public ICollection<Timeslot> TimeSlots { get; set; }
+        public List<Timeslot> TimeSlots { get; set; } = new();
         public DateTime CreatedAt { get; set; }
     }
 }
