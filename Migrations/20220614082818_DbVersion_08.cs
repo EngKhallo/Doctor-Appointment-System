9c2de16295a37aac86212dabb66ddeb6a72085a8
EngKhallo <engkhalid.ict@gmail.com>
2022-06-15 10:28:36 +0300

    New - Migration of timeSlots

---
+++ Migrations/20220614082818_DbVersion_08.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Doctor_Appointment_System.Migrations
+{
+    public partial class DbVersion_08 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "StartTime",
+                table: "Timeslots",
+                type: "interval",
+                nullable: false,
+                oldClrType: typeof(TimeOnly),
+                oldType: "time without time zone");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "EndTime",
+                table: "Timeslots",
+                type: "interval",
+                nullable: false,
+                oldClrType: typeof(TimeOnly),
+                oldType: "time without time zone");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "StartTime",
+                table: "Timeslots",
+                type: "time without time zone",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "interval");
+
+            migrationBuilder.AlterColumn<TimeOnly>(
+                name: "EndTime",
+                table: "Timeslots",
+                type: "time without time zone",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "interval");
+        }
+    }
+}
