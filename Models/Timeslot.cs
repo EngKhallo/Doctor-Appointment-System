d183f01c3cb9380905b1df93b83704e1c138a744
EngKhallo <engkhalid.ict@gmail.com>
2022-06-15 10:26:55 +0300

    update

--- Models/Timeslot.cs
+++ Models/Timeslot.cs
@@ -2,11 +2,16 @@ namespace Doctor_Appointment_System.Models;
     public class Timeslot
     {
         public int Id { get; set; }
+
         public int ScheduleId { get; set; }
         public Schedule Schedule { get; set; } = null!;
+
         public TimeSpan StartTime { get; set; }
         public TimeSpan EndTime { get; set; }
+
         public string Description { get; set; }= "";
+
         public int MaxAppointments { get; set; }
+        public List<Booking> Bookings { get; set; } = new();
         public DateTime CreatedAt { get; set; }
     }
