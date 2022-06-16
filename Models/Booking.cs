964f25d5dd070016531e3d532e11326049941bc0
EngKhallo <engkhalid.ict@gmail.com>
2022-06-17 00:52:08 +0300

    update

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -2,11 +2,15 @@ namespace Doctor_Appointment_System.Models;
 public class Booking
 {
     public int Id { get; set; }
+
     public int UserId { get; set; }
     public User? User { get; set; }
+
     public DateTime AppointmentTime { get; set; }
+
     public int TimeslotId { get; set; }
     public Timeslot Timeslot { get; set; } = null!;
+    
     public decimal PaidAmount { get; set; }
     public decimal Commission { get; set; }
     public decimal DoctorRevenue { get; set; }
