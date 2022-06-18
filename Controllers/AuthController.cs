c01f104ee68677cb972371c31ad6c1f74d031fcc
EngKhallo <engkhalid.ict@gmail.com>
2022-06-19 00:48:54 +0300

    Token Login

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,8 +1,40 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Doctor_Appointment_System.Controllers;
 [Route("api/v1/[controller]")]
 public class AuthController : ControllerBase
 {
+    // POST /auth/login : Username and password
+    [HttpPost("login")]
+    public IActionResult Login()
+    {
+        var now = DateTime.Now;
 
+        var claims = new List<Claim>
+        {
+            new("exp", "0"),
+            new("sub", "1"),
+            new("FullName", "User Full Name")
+        };
+
+
+        var KeyInput = "random_text_with_atleast_32_chars";
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KeyInput));
+
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken("MyAPI", "MyFrontendApp", claims, now, now.AddHours(1), credentials);
+
+        var handler = new JwtSecurityTokenHandler();
+        var jwt = handler.WriteToken(token);
+        var result = new
+        {
+            token = jwt
+        };
+        return Ok(result);
+    }
 }
