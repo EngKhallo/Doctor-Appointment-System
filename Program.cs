dcbe1f2a56aa0e815e0d4d64a61d2686ba98459a
EngKhallo <engkhalid.ict@gmail.com>
2022-06-25 15:28:32 +0300

    JWTBearer, UserAuths

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-using System.Text;
-using System.Text.Json.Serialization;
-using Data;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,28 +11,29 @@ builder.Services.AddDbContext<AppointmentsDbContext>(config =>
                 config.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
 
 // Add services to the container.
-
 builder.Services.AddControllers()
-    .AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+	.AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
-builder.Services.AddAuthentication("Bearer")
-                .AddJwtBearer(config =>
-                {
-                    config.RequireHttpsMetadata = false;
 
-                    var KeyInput = "random_text_with_atleast_32_chars";
-                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KeyInput));
+builder.Services.AddAuthentication("Bearer") // Token: JWT = Json Web Token
+    .AddJwtBearer(config =>
+    {
+        config.RequireHttpsMetadata = false;
 
-                    config.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidIssuer = "MyAPI",
-                        ValidateAudience = true,
-                        ValidAudience = "MyFrontendApp",
-                        ValidateLifetime = true, // Expiration
-                        IssuerSigningKey = key
-                    };
-                });
+        var keyInput = "random_text_with_at_least_32_chars";
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(keyInput));
+
+        config.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = "MyAPI",
+            ValidateAudience = true,
+            ValidAudience = "MyFrontendApp",
+            ValidateLifetime = true,
+            IssuerSigningKey = key
+        };
+    });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -46,7 +48,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
