794694cabb5b3744e7d09f2cb3e33a5b164281ad
EngKhallo <engkhalid.ict@gmail.com>
2022-06-28 15:52:58 +0300

    Get Logged User from Session

--- Helpers/UserExtension.cs
+++ Helpers/UserExtension.cs
@@ -1,10 +1,18 @@
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Doctor_Appointment_System.Helpers;
-public class UserExtension
+public static class UserExtension
 {
-    public static int GetId(ClaimsPrincipal principal)
+    public static int GetId(this ClaimsPrincipal principal) // this keyword : extension, inherits every class
     {
+         var isConversionSuccessful = int.TryParse(principal.FindFirstValue(JwtRegisteredClaimNames.Sub), out var userId); // whole user claim : id with its info
+        if (isConversionSuccessful)
+        {
+            return userId;
+        }
+
+        return 0;
     }
 }
