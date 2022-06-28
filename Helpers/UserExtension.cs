3d3410e1190a93f5fb256095c4001e747ba400df
EngKhallo <engkhalid.ict@gmail.com>
2022-06-28 15:46:28 +0300

    UserExtension Class

---
+++ Helpers/UserExtension.cs
@@ -0,0 +1,10 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Doctor_Appointment_System.Helpers;
+public class UserExtension
+{
+    public static int GetId(ClaimsPrincipal principal)
+    {
+    }
+}
